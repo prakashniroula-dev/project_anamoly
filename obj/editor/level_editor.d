27f8f5907a4a4aba127c8c98fdfbc48864b5c206
Prakash Niroula <prakashniroula.dev@gmail.com>
2026-08-04 13:49:10 +0545

    refresh

---
+++ obj/editor/level_editor.d
@@ -0,0 +1,150 @@
+obj/editor/level_editor.o: src/editor/level_editor.cpp \
+ src/core/constants.hpp src/graphics/tiles.hpp \
+ SFML/include/SFML/Graphics/Sprite.hpp \
+ SFML/include/SFML/Graphics/Export.hpp SFML/include/SFML/Config.hpp \
+ SFML/include/SFML/Graphics/Drawable.hpp \
+ SFML/include/SFML/Graphics/Rect.hpp SFML/include/SFML/System/Vector2.hpp \
+ SFML/include/SFML/System/Angle.hpp SFML/include/SFML/System/Angle.inl \
+ SFML/include/SFML/System/Vector2.inl SFML/include/SFML/Graphics/Rect.inl \
+ SFML/include/SFML/Graphics/RenderStates.hpp \
+ SFML/include/SFML/Graphics/BlendMode.hpp \
+ SFML/include/SFML/Graphics/CoordinateType.hpp \
+ SFML/include/SFML/Graphics/StencilMode.hpp \
+ SFML/include/SFML/Graphics/Transform.hpp \
+ SFML/include/SFML/Graphics/Transform.inl \
+ SFML/include/SFML/Graphics/Transformable.hpp \
+ SFML/include/SFML/Graphics/Vertex.hpp \
+ SFML/include/SFML/Graphics/Color.hpp \
+ SFML/include/SFML/Graphics/Color.inl src/editor/level_editor.hpp \
+ SFML/include/SFML/Graphics.hpp \
+ SFML/include/SFML/Graphics/CircleShape.hpp \
+ SFML/include/SFML/Graphics/Shape.hpp \
+ SFML/include/SFML/Graphics/PrimitiveType.hpp \
+ SFML/include/SFML/Graphics/VertexArray.hpp \
+ SFML/include/SFML/Graphics/ConvexShape.hpp \
+ SFML/include/SFML/Graphics/Font.hpp SFML/include/SFML/Graphics/Glyph.hpp \
+ SFML/include/SFML/Graphics/Texture.hpp \
+ SFML/include/SFML/Window/GlResource.hpp \
+ SFML/include/SFML/Window/Export.hpp SFML/include/SFML/Graphics/Image.hpp \
+ SFML/include/SFML/Graphics/RectangleShape.hpp \
+ SFML/include/SFML/Graphics/RenderTarget.hpp \
+ SFML/include/SFML/Graphics/View.hpp \
+ SFML/include/SFML/Graphics/RenderTexture.hpp \
+ SFML/include/SFML/Window/ContextSettings.hpp \
+ SFML/include/SFML/Graphics/RenderWindow.hpp \
+ SFML/include/SFML/Window/VideoMode.hpp \
+ SFML/include/SFML/Window/Window.hpp \
+ SFML/include/SFML/Window/WindowBase.hpp \
+ SFML/include/SFML/Window/Vulkan.hpp \
+ SFML/include/SFML/Window/WindowEnums.hpp \
+ SFML/include/SFML/Window/WindowHandle.hpp \
+ SFML/include/SFML/System/Time.hpp SFML/include/SFML/System/Time.inl \
+ SFML/include/SFML/Window/WindowBase.inl \
+ SFML/include/SFML/Window/Event.hpp SFML/include/SFML/Window/Joystick.hpp \
+ SFML/include/SFML/System/String.hpp SFML/include/SFML/System/Export.hpp \
+ SFML/include/SFML/System/Utf.hpp SFML/include/SFML/System/Utf.inl \
+ SFML/include/SFML/System/Err.hpp SFML/include/SFML/System/String.inl \
+ SFML/include/SFML/Window/Keyboard.hpp SFML/include/SFML/Window/Mouse.hpp \
+ SFML/include/SFML/Window/Sensor.hpp SFML/include/SFML/System/Vector3.hpp \
+ SFML/include/SFML/System/Vector3.inl SFML/include/SFML/Window/Event.inl \
+ SFML/include/SFML/System/Clock.hpp SFML/include/SFML/Graphics/Shader.hpp \
+ SFML/include/SFML/Graphics/Glsl.hpp SFML/include/SFML/Graphics/Glsl.inl \
+ SFML/include/SFML/Graphics/Text.hpp \
+ SFML/include/SFML/Graphics/VertexBuffer.hpp SFML/include/SFML/Window.hpp \
+ SFML/include/SFML/Window/Clipboard.hpp \
+ SFML/include/SFML/Window/Context.hpp SFML/include/SFML/Window/Cursor.hpp \
+ SFML/include/SFML/Window/Touch.hpp SFML/include/SFML/System.hpp \
+ SFML/include/SFML/System/Exception.hpp \
+ SFML/include/SFML/System/FileInputStream.hpp \
+ SFML/include/SFML/System/InputStream.hpp \
+ SFML/include/SFML/System/MemoryInputStream.hpp \
+ SFML/include/SFML/System/Sleep.hpp \
+ SFML/include/SFML/System/TimeoutWithPredicate.hpp \
+ SFML/include/SFML/System/Version.hpp src/entities/terrain.hpp \
+ src/core/scale.hpp src/debug/logs.hpp
+src/core/constants.hpp:
+src/graphics/tiles.hpp:
+SFML/include/SFML/Graphics/Sprite.hpp:
+SFML/include/SFML/Graphics/Export.hpp:
+SFML/include/SFML/Config.hpp:
+SFML/include/SFML/Graphics/Drawable.hpp:
+SFML/include/SFML/Graphics/Rect.hpp:
+SFML/include/SFML/System/Vector2.hpp:
+SFML/include/SFML/System/Angle.hpp:
+SFML/include/SFML/System/Angle.inl:
+SFML/include/SFML/System/Vector2.inl:
+SFML/include/SFML/Graphics/Rect.inl:
+SFML/include/SFML/Graphics/RenderStates.hpp:
+SFML/include/SFML/Graphics/BlendMode.hpp:
+SFML/include/SFML/Graphics/CoordinateType.hpp:
+SFML/include/SFML/Graphics/StencilMode.hpp:
+SFML/include/SFML/Graphics/Transform.hpp:
+SFML/include/SFML/Graphics/Transform.inl:
+SFML/include/SFML/Graphics/Transformable.hpp:
+SFML/include/SFML/Graphics/Vertex.hpp:
+SFML/include/SFML/Graphics/Color.hpp:
+SFML/include/SFML/Graphics/Color.inl:
+src/editor/level_editor.hpp:
+SFML/include/SFML/Graphics.hpp:
+SFML/include/SFML/Graphics/CircleShape.hpp:
+SFML/include/SFML/Graphics/Shape.hpp:
+SFML/include/SFML/Graphics/PrimitiveType.hpp:
+SFML/include/SFML/Graphics/VertexArray.hpp:
+SFML/include/SFML/Graphics/ConvexShape.hpp:
+SFML/include/SFML/Graphics/Font.hpp:
+SFML/include/SFML/Graphics/Glyph.hpp:
+SFML/include/SFML/Graphics/Texture.hpp:
+SFML/include/SFML/Window/GlResource.hpp:
+SFML/include/SFML/Window/Export.hpp:
+SFML/include/SFML/Graphics/Image.hpp:
+SFML/include/SFML/Graphics/RectangleShape.hpp:
+SFML/include/SFML/Graphics/RenderTarget.hpp:
+SFML/include/SFML/Graphics/View.hpp:
+SFML/include/SFML/Graphics/RenderTexture.hpp:
+SFML/include/SFML/Window/ContextSettings.hpp:
+SFML/include/SFML/Graphics/RenderWindow.hpp:
+SFML/include/SFML/Window/VideoMode.hpp:
+SFML/include/SFML/Window/Window.hpp:
+SFML/include/SFML/Window/WindowBase.hpp:
+SFML/include/SFML/Window/Vulkan.hpp:
+SFML/include/SFML/Window/WindowEnums.hpp:
+SFML/include/SFML/Window/WindowHandle.hpp:
+SFML/include/SFML/System/Time.hpp:
+SFML/include/SFML/System/Time.inl:
+SFML/include/SFML/Window/WindowBase.inl:
+SFML/include/SFML/Window/Event.hpp:
+SFML/include/SFML/Window/Joystick.hpp:
+SFML/include/SFML/System/String.hpp:
+SFML/include/SFML/System/Export.hpp:
+SFML/include/SFML/System/Utf.hpp:
+SFML/include/SFML/System/Utf.inl:
+SFML/include/SFML/System/Err.hpp:
+SFML/include/SFML/System/String.inl:
+SFML/include/SFML/Window/Keyboard.hpp:
+SFML/include/SFML/Window/Mouse.hpp:
+SFML/include/SFML/Window/Sensor.hpp:
+SFML/include/SFML/System/Vector3.hpp:
+SFML/include/SFML/System/Vector3.inl:
+SFML/include/SFML/Window/Event.inl:
+SFML/include/SFML/System/Clock.hpp:
+SFML/include/SFML/Graphics/Shader.hpp:
+SFML/include/SFML/Graphics/Glsl.hpp:
+SFML/include/SFML/Graphics/Glsl.inl:
+SFML/include/SFML/Graphics/Text.hpp:
+SFML/include/SFML/Graphics/VertexBuffer.hpp:
+SFML/include/SFML/Window.hpp:
+SFML/include/SFML/Window/Clipboard.hpp:
+SFML/include/SFML/Window/Context.hpp:
+SFML/include/SFML/Window/Cursor.hpp:
+SFML/include/SFML/Window/Touch.hpp:
+SFML/include/SFML/System.hpp:
+SFML/include/SFML/System/Exception.hpp:
+SFML/include/SFML/System/FileInputStream.hpp:
+SFML/include/SFML/System/InputStream.hpp:
+SFML/include/SFML/System/MemoryInputStream.hpp:
+SFML/include/SFML/System/Sleep.hpp:
+SFML/include/SFML/System/TimeoutWithPredicate.hpp:
+SFML/include/SFML/System/Version.hpp:
+src/entities/terrain.hpp:
+src/core/scale.hpp:
+src/debug/logs.hpp:
